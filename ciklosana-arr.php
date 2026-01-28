a209fa14d955b5a964eecdfbb6ef95c3c16ca3c8
DeivisStasuls <deivis.stasuls@gmail.com>
2026-01-28 09:04:39 +0200

    ceturtais uzd

---
+++ ciklosana-arr.php
@@ -0,0 +1,22 @@
+<?php
+
+$students =
+[
+    ["Students"=>1, "Vārds"=> "Džons","Vecums"=>28, "Atzīmes"=>[90,85,88]],
+    ["Students"=>2, "Vārds"=>"Džena","Vecums"=>22, "Atzīmes"=>[92,80,84]],
+    ["Students"=>3, "Vārds"=>"Džo", "Vecums"=>21, "Atzīmes"=>[78,85,90]],
+];
+
+foreach ( $students as $biden){
+        $avarage = 0;
+        echo $biden["Vārds"]." ";
+
+        echo $biden["Vecums"]." ";
+
+        foreach($biden["Atzīmes"] as $num){
+            $avarage =+$num;
+        }
+        echo $avarage / count($biden["Atzīmes"]).PHP_EOL;
+
+        
+}
