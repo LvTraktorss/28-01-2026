e0ba8210a03231f11a0e500792ca0edb325f22ae
DeivisStasuls <deivis.stasuls@gmail.com>
2026-01-28 11:09:28 +0200

    septitais uzd

---
+++ 7uzd.php
@@ -0,0 +1,9 @@
+<?php
+
+$persons=
+[
+    ["name"=> "Jānis", "age"=>20, "e-pasts"=>"janis@janis.lv", "valodas"=>"PHP, Python, Java"],
+    ["name"=> "Antra", "age"=>34, "e-pasts"=>"antra@antra.com", "valodas"=>"HTML, Javascript, SQL"]
+
+];
+
